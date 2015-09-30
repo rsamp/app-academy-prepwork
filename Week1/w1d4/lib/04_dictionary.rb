7a63c2e260ffca4e9dc75ac0d90db930b9ee0522
Ryan Samp <rsamp@ryansamp.com>
2015-09-29 21:55:39 -0700

    Finish ex 04

--- Week1/w1d4/lib/04_dictionary.rb
+++ Week1/w1d4/lib/04_dictionary.rb
@@ -1,3 +1,46 @@
 class Dictionary
   # TODO: your code goes here!
+  def initialize
+    @dictionary = {}
+  end
+
+  def entries
+    @dictionary
+  end
+
+  def add(definition)
+    @dictionary.merge!(definition) if definition.class == Hash
+    @dictionary[definition] = nil if definition.class == String
+  end
+
+  def keywords
+    @dictionary.keys
+  end
+
+  def include?(word)
+    return true if @dictionary.include?(word)
+    false
+  end
+
+  def find(letters)
+    matching_definitions = {}
+    @dictionary.each_key do |word|
+      if word.start_with?(letters)
+        matching_definitions.merge!({word => @dictionary[word]})
+      end
+    end
+    matching_definitions
+  end
+
+  def keywords
+    @dictionary.keys.sort
+  end
+
+  def printable
+    printed = ""
+    keywords.each do |word|
+      printed+="[#{word}] \"#{@dictionary[word]}\"\n"
+    end
+    printed.chomp
+  end
 end
