e31136f8f05062e584376495bd3479d9fbfd3bd5
Ryan Samp <rsamp@ryansamp.com>
2015-10-14 16:41:43 -0700

    Work on Battleship game

--- Week3/w3d3/lib/battleship.rb
+++ Week3/w3d3/lib/battleship.rb
@@ -1,3 +1,29 @@
+require "board"
+require "player"
+
 class BattleshipGame
   attr_reader :board, :player
+
+  def initialize(player = HumanPlayer.new("Jeff"), board = Board.random)
+  # def initialze(player, board)
+    @player = player
+    @board = board
+    @hit = false
+  end
+
+  def attack(space)
+    board[space] = :x
+  end
+
+  def count
+    board.count
+  end
+
+  def game_over?
+    board.won?
+  end
+
+  def play_turn
+    
+  end
 end
--- Week3/w3d3/lib/board.rb
+++ Week3/w3d3/lib/board.rb
@@ -1,2 +1,45 @@
 class Board
+  attr_reader :grid
+
+  def self.default_grid
+    Array.new(10) { Array.new(10) }
+  end
+
+  def initialize(grid=self.class.default_grid)
+    @grid = grid
+  end
+
+  def [](pos)
+    grid[pos[0]][pos[1]]
+  end
+
+  def []=(pos,sym)
+    grid[pos[0]][pos[1]] = sym
+  end
+
+  def count
+    grid.flatten.count(:s)
+  end
+
+  def empty?(space=nil)
+    return count==0 if space.nil?
+    self[space].nil?
+  end
+
+  def full?
+    size = grid.flatten.length
+    count == size
+  end
+
+  def place_random_ship
+    raise "board full" if full?
+    row = rand(grid.length)
+    column = rand(grid[0].length)
+    pos = [row, column]
+    self[pos] = :s
+  end
+
+  def won?
+    count == 0
+  end
 end
